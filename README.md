3f8460308848a2d64b43305971c7de1330dec20f
tantanGH <tantangh68@gmail.com>
2023-01-04 12:26:34 +0900

    first implementation

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # pngdeband
 24bit PNG to 15bit PNG converter with de-banding filter
+
+Python で書かれた PNG 画像 de-banding フィルタツールです。
+
+24bit color RGB/RGBA の PNG ファイルを X680x0 で表示するのに適した 15bit color の PNG に変換します。
+この時、単純に RGB 各 8bit の下位 3bit を落として 24bit から 15bit に変換してしまうと、バンド(マッハバンド)がすごく目立ちます。
+これを防ぐために debanding (バンド除去) フィルターをかけつつ 15bit に変換し、新たな PNG ファイルとして出力します。
+
+PNG ファイルはその仕様上 15bit/16bit のビット深度はサポートされていませんので 24bit RGB PNG として出力しますが、
+使われているカラーは 15bit 分のみ、下位3bitはすべて0になっています。
+
+---
+
+### ffmpeg のインストール
+
+インストールにあたっては、オープンソースの動画フィルタコマンドラインツール `ffmpeg` の導入が必要になります。
+やや大きめの規模のソフトウェアになりますので注意してください。
+
+Windowsの場合は公式サイト [https://ffmpeg.org/](https://ffmpeg.org/) に行き、Get packages & executable files を選びます。
+
+![](images/ffmpeg1.png)
+
+下の方のサイト(BtbN)が分かりやすいのでそちらから、
+
+    ffmpeg-master-latest-win64-gpl-shared.zip
+
+をダウンロードします。Windowsインストーラ形式にはなっていないので、zipファイルを自分の希望する場所に展開します。
+その後で bin フォルダを環境変数 PATH に追加してください。
+
+ググると詳細な導入説明サイトが沢山ありますので、それらも参考にしてください。
+
+macOSの場合は
+
+    brew install ffmpeg
+
+だけです。
+
+---
+
+### pngdeband のインストール
+
+    pip install git+https://github.com/tantanGH/pngdeband.git
+
+[Windowsユーザ向けPython導入ガイド](https://github.com/tantanGH/distribution/blob/main/windows_python_for_x68k.md)
+
+---
+
+### 使い方
+
+    pngdeband [options] <input-png-file> <output-png-file>
+
+---
+
+### 変換例
+
--- pngdeband/pngdeband.py
+++ pngdeband/pngdeband.py
@@ -1,144 +1,59 @@
 import argparse
+import tempfile
+import os
+import uuid
+import ffmpeg
 
-from PIL import Image
+def deband_png(in_file, out_file, resize, width, height, th1, th2, th3, blur):
 
-def convert_png_to_sp(png_file,out_file,out_format='r',size=(16,16),aspect=None):
+  with tempfile.TemporaryDirectory() as tmp_dir:
+  
+    tmp_bmp_file = os.path.join(tmp_dir,str(uuid.uuid1())+".bmp")
 
-    # resize
-    raw_image = Image.open(png_file)
-    if aspect:
-      aspect_size = ( raw_image.width * 2 // 3, raw_image.height )
-      raw_image = raw_image.resize(aspect_size)
+    (
+      ffmpeg
+      .input(in_file, vcodec='png')
+      .video
+      .filter("deband",blur=blur)
+      .output(tmp_bmp_file, vcodec='bmp', pix_fmt='rgb555')
+      .global_args('-hide_banner','-loglevel','error')
+      .run()
+    )
 
-    resize_image = raw_image.convert('RGBA').resize(size)
-    resize_image_bytes = resize_image.tobytes()
-
-    # extract alpha channelq
-    alpha = []
-    for i,b in enumerate(resize_image_bytes):
-        if (i % 4) == 3:
-            if (b < 32):            # transparent alpha threshould
-                p = 0
-            else:
-                p = -1
-            alpha.append(p)
-
-    # color reduction
-    color15_image = resize_image.quantize(colors=15)
-    color15_image_bytes = color15_image.tobytes()
-    color15_image_pallet = color15_image.getpalette()
-
-    # generate sprite pattern array data
-    pattern_array = []
-    for i,b in enumerate(color15_image_bytes):
-        if (alpha[i] == 0):
-            pattern_array.append(0)
-        else:
-            pattern_array.append(b+1)
-
-#    for y in range(size[1]):
-#        s = [str(n) for n in pattern_array[ y * size[0] : y * size[0] + size[0] ]]
-#        print(",".join(s))
-
-    # how many 16x16 patterns do we need?
-    patterns_x = ( size[0] - 1 ) // 16 + 1
-    patterns_y = ( size[1] - 1 ) // 16 + 1
-
-    # output lines
-    output_lines = []
-
-    if out_format != "b":
-        output_lines.append("")
-        output_lines.append(f"unsigned short sp_pattern_data[] = {{")
-
-    # generate patterns by 16x16
-    for py in range(patterns_y):
-
-        for px in range(patterns_x):
-
-            pattern_id = py * patterns_x + px
-
-            width = size[0]
-            ofs = py * width * 16 + px * 16
-
-            if out_format == "b":
-
-                output_lines.append("")
-                output_lines.append(f"/* sprite pattern data {pattern_id} */")
-                output_lines.append(f"unsigned char sp_pattern_data_{pattern_id}[] = {{")
-
-                # output pattern array data
-                for y in range(16):
-                    s = [str(n) for n in pattern_array[ ofs + y * width : ofs + y * width + 16 ]]
-                    output_lines.append("    " + ",".join(s) + ",")
-
-                output_lines.append("};")
-
-            else:
-
-                # convert dot data to raw pattern data
-                pattern_data = [ 0 ] * 128
-                for y in range(8):
-                    for x in range(4):
-                        pattern_data[ 0x00 + y*4 + x ] = ( pattern_array[ ofs + (y+0) * width + (x+0)*2 ] << 4 ) | ( pattern_array[ ofs + (y+0) * width + (x+0)*2 + 1])
-                        pattern_data[ 0x20 + y*4 + x ] = ( pattern_array[ ofs + (y+8) * width + (x+0)*2 ] << 4 ) | ( pattern_array[ ofs + (y+8) * width + (x+0)*2 + 1])
-                        pattern_data[ 0x40 + y*4 + x ] = ( pattern_array[ ofs + (y+0) * width + (x+4)*2 ] << 4 ) | ( pattern_array[ ofs + (y+0) * width + (x+4)*2 + 1])
-                        pattern_data[ 0x60 + y*4 + x ] = ( pattern_array[ ofs + (y+8) * width + (x+4)*2 ] << 4 ) | ( pattern_array[ ofs + (y+8) * width + (x+4)*2 + 1])
-
-                # output raw pattern data
-                output_lines.append("")
-                output_lines.append(f"    /* sprite pattern data {pattern_id} */")
-                for y in range(8):
-                    tuples = [ tuple( pattern_data[ y * 16 + x * 2 : y * 16 + x * 2 + 2 ]) for x in range(8)]
-                    s = [ '0x' + format(t[0],'02x') + format(t[1],'02x') for t in tuples ]
-                    output_lines.append("    " + ",".join(s) + ",")
-
-    if out_format != "b":
-        output_lines.append("};")
-
-    # generate palette data
-    palette_data = []
-    palette_data.append(0)      # palette 0 = transparent code
-    for i in range(15):
-        r = color15_image_pallet[ i * 3 + 0 ]
-        g = color15_image_pallet[ i * 3 + 1 ]
-        b = color15_image_pallet[ i * 3 + 2 ]
-        rgb555 = ((g>>3)<<11) | ((r>>3)<<6) | ((b>>3)<<1) | 1
-        palette_data.append(rgb555)
-
-    # output palette data
-    output_lines.append("")
-    output_lines.append("/* palette data */")
-    output_lines.append("unsigned short sp_palette_data[] = {")
-    s = [ '0x' + format(p,'04x') for p in palette_data ]
-    output_lines.append("    " + ",".join(s) + ",")
-    output_lines.append("};")
-    output_lines.append("")
-
-    # output to file or stdout
-    if out_file == None:
-        for l in output_lines:
-            print(l)
-    else:
-        with open(out_file, "w") as f:
-            for l in output_lines:
-                f.write(l+"\n")
+    (
+      ffmpeg
+      .input(tmp_bmp_file, vcodec='bmp')
+      .video
+      .output(out_file, vcodec='png', pix_fmt='rgb24')
+      .global_args('-hide_banner','-loglevel','error')
+      .run()
+    )
 
 def main():
 
     parser = argparse.ArgumentParser()
 
     parser.add_argument("infile",help="input PNG file")
-    parser.add_argument("-o","--outfile",help="output file")
-    parser.add_argument("-f","--format",help="output format (b:X-BASIC, r:raw)",default="r")
-    parser.add_argument("-x","--width",help="sprite width (default:16)",type=int,default=16)
-    parser.add_argument("-y","--height",help="sprite height (default:16)",type=int,default=16)
-    parser.add_argument("-a","--aspect",help="resize source image to 2:3",action='store_true',default=False)
+    parser.add_argument("outfile",help="output PNG file")
+    parser.add_argument("-r","--resize",help="resize percent",type=int)
+    parser.add_argument("-x","--width",help="resize width",type=int)
+    parser.add_argument("-y","--height",help="resize height",type=int)
+    parser.add_argument("-t1","--threshold1",help="debanding detection threshold for channel 1 (default:0.02)",default=0.02)
+    parser.add_argument("-t2","--threshold2",help="debanding detection threshold for channel 2 (default:0.02)",default=0.02)
+    parser.add_argument("-t3","--threshold3",help="debanding detection threshold for channel 3 (default:0.02)",default=0.02)
+    parser.add_argument("-bl","--blur",help="1:check with average of 4 pixels 0:check with all 4 pixels (default:1)",type=int,default=1)
 
     args = parser.parse_args()
 
-    convert_png_to_sp(args.infile,args.outfile,args.format,(args.width,args.height),args.aspect)
-
+    deband_png(args.infile, \
+               args.outfile, \
+               args.resize, \
+               args.width, \
+               args.height, \
+               args.threshold1, \
+               args.threshold2, \
+               args.threshold3, \
+               args.blur)
 
 if __name__ == "__main__":
     main()
