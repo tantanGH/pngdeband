fdd4484e00ce4d466dd48ae1387df01e5bcdb07c
tantanGH <tantangh68@gmail.com>
2023-01-04 14:47:55 +0900

    resize option

--- README.md
+++ README.md
@@ -42,7 +42,11 @@ Windowsの場合は公式サイト [https://ffmpeg.org/](https://ffmpeg.org/) �
 
 pip を使って導入します。
 
+    pip uninstall ffmpeg
     pip install git+https://github.com/tantanGH/pngdeband.git
+
+`ffmpeg` というライブラリをアンインストールする必要があります。非常に紛らわしいですが、このライブラリは使えません。
+`pngdeband` をインストールすると `ffmpeg-python` という本来のラッパーライブラリが導入されます。
 
 [Windowsユーザ向けPython導入ガイド](https://github.com/tantanGH/distribution/blob/main/windows_python_for_x68k.md)
 
--- pngdeband/pngdeband.py
+++ pngdeband/pngdeband.py
@@ -8,15 +8,42 @@ def deband_png(in_file, out_file, resize, width, height, th1, th2, th3, blur):
 
   with tempfile.TemporaryDirectory() as tmp_dir:
   
+
+    meta_data = ffmpeg.probe(in_file)
+
+    # Get the video stream information
+    video_stream = next(s for s in meta_data['streams'] if s['codec_type'] == 'video')
+
+    # Get the original width and height of the video
+    width_orig = int(video_stream['width'])
+    height_orig = int(video_stream['height'])
+
+    if width is not None and height is not None:
+      width_new = width
+      height_new = height
+    elif width is not None and height is None:
+      width_new = width
+      height_new = height_orig * width_new // width_orig
+    elif width is None and height is not None:
+      height_new = height
+      width_new = width_orig * height_new // height_orig
+    elif resize is not None:
+      width_new = width_orig * resize // 100
+      height_new = height_orig * resize // 100
+    else:
+      width_new = width_orig
+      height_new = height_orig
+
     tmp_bmp_file = os.path.join(tmp_dir,str(uuid.uuid1())+".bmp")
 
+#      .filter("deband", "1thr"={th1}, "2thr"={th2}, "3thr"={th3}, blur={blur}")
     (
       ffmpeg
       .input(in_file, vcodec='png')
       .video
-      .filter("deband",blur=blur)
+      .filter("scale", width_new, height_new)
       .output(tmp_bmp_file, vcodec='bmp', pix_fmt='rgb555')
-      .global_args('-hide_banner','-loglevel','error')
+      .global_args('-hide_banner','-loglevel','error','-vf',f"deband=1thr={th1}:2thr={th2}:3thr={th3}:blur={blur}")
       .run()
     )
 
@@ -38,9 +65,9 @@ def main():
     parser.add_argument("-r","--resize",help="resize percent",type=int)
     parser.add_argument("-x","--width",help="resize width",type=int)
     parser.add_argument("-y","--height",help="resize height",type=int)
-    parser.add_argument("-t1","--threshold1",help="debanding detection threshold for channel 1 (default:0.02)",default=0.02)
-    parser.add_argument("-t2","--threshold2",help="debanding detection threshold for channel 2 (default:0.02)",default=0.02)
-    parser.add_argument("-t3","--threshold3",help="debanding detection threshold for channel 3 (default:0.02)",default=0.02)
+    parser.add_argument("-t1","--threshold1",help="band detection threshold for channel 1 (0.00003 to 0.5, default:0.02)",default=0.02)
+    parser.add_argument("-t2","--threshold2",help="band detection threshold for channel 2 (0.00003 to 0.5, default:0.02)",default=0.02)
+    parser.add_argument("-t3","--threshold3",help="band detection threshold for channel 3 (0.00003 to 0.5, default:0.02)",default=0.02)
     parser.add_argument("-bl","--blur",help="1:check with average of 4 pixels 0:check with all 4 pixels (default:1)",type=int,default=1)
 
     args = parser.parse_args()
