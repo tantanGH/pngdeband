af908f6b557bc3531bff243b2b7374e46200d64d
tantanGH <tantangh68@gmail.com>
2023-05-24 23:23:39 +0900

    cas filtering

--- pngdeband/pngdeband.py
+++ pngdeband/pngdeband.py
@@ -4,7 +4,7 @@ import os
 import uuid
 import ffmpeg
 
-def deband_png(in_file, out_file, resize, width, height, th1, th2, th3, blur):
+def deband_png(in_file, out_file, resize, width, height, sharp, th1, th2, th3, blur):
 
   with tempfile.TemporaryDirectory() as tmp_dir:
   
@@ -42,6 +42,7 @@ def deband_png(in_file, out_file, resize, width, height, th1, th2, th3, blur):
       .input(in_file, vcodec='png')
       .video
       .filter("scale", width_new, height_new)
+      .filter("cas", sharp)
       .output(tmp_bmp_file, vcodec='bmp', pix_fmt='rgb555')
       .global_args('-hide_banner','-loglevel','error','-vf',f"deband=1thr={th1}:2thr={th2}:3thr={th3}:blur={blur}")
       .run()
@@ -65,6 +66,7 @@ def main():
     parser.add_argument("-x","--width",help="resize width",type=int)
     parser.add_argument("-y","--height",help="resize height",type=int)
     parser.add_argument("-r","--resize",help="resize percent",type=int)
+    parser.add_argument("-s","--sharpness",help="sharpness ratio (0.0 to 1.0, default:0.0)",type=float,default=0.0)
     parser.add_argument("-t1","--threshold1",help="band detection threshold for channel 1 (0.00003 to 0.5, default:0.02)",default=0.02)
     parser.add_argument("-t2","--threshold2",help="band detection threshold for channel 2 (0.00003 to 0.5, default:0.02)",default=0.02)
     parser.add_argument("-t3","--threshold3",help="band detection threshold for channel 3 (0.00003 to 0.5, default:0.02)",default=0.02)
@@ -77,6 +79,7 @@ def main():
                args.resize, \
                args.width, \
                args.height, \
+               args.sharpness, \
                args.threshold1, \
                args.threshold2, \
                args.threshold3, \
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pngdeband",
-    version="0.0.3",
+    version="0.1.0",
     author="tantanGH",
     author_email="tantanGH@github",
     license='MIT',
